d52ec40f57dee6341bff25725690945ed88c25c8
alanHarper123 <1531508001@qq.com>
2018-07-20 12:11:58 +0800

    completed redis messaging

--- src/main/java/com/qiujintao/spring_boot_Redis_demo_2/SpringBootRedisDemo2Application.java
+++ src/main/java/com/qiujintao/spring_boot_Redis_demo_2/SpringBootRedisDemo2Application.java
@@ -1,12 +1,28 @@
 package com.qiujintao.spring_boot_Redis_demo_2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.qiujintao.spring_boot_Redis_demo_2.queue.MessagePublisher;
+
 @SpringBootApplication
-public class SpringBootRedisDemo2Application {
+public class SpringBootRedisDemo2Application implements CommandLineRunner {
+	@Autowired
+	private MessagePublisher messagePublisher;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRedisDemo2Application.class, args);
+	}
+
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		messagePublisher.publish("test 1");
+		messagePublisher.publish("test 2");
+		messagePublisher.publish("test 3");
+		
 	}
 }
--- src/main/java/com/qiujintao/spring_boot_Redis_demo_2/queue/MessagePublisherImpl.java
+++ src/main/java/com/qiujintao/spring_boot_Redis_demo_2/queue/MessagePublisherImpl.java
@@ -1,16 +1,16 @@
 package com.qiujintao.spring_boot_Redis_demo_2.queue;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.stereotype.Service;
 
-@Service
+
+
 public class MessagePublisherImpl implements MessagePublisher {
 	
-	@Autowired
+
 	private RedisTemplate<String, Object> redisTemplate;
-	@Autowired
+
 	private ChannelTopic topic;
 	public MessagePublisherImpl() {
 	}
